691a2af931919be98e1e87c302c24a9d871e9e10
papu_68 <giorgobianipaul@gmail.com>
2024-09-10 11:36:32 +0200

    created swagger documentaion for API

--- SkillXchange/urls.py
+++ SkillXchange/urls.py
@@ -2,9 +2,13 @@
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework.authtoken.views import obtain_auth_token
+from .yasg import urlpatterns as doc_urls
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('skillapp.urls')),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
+
+urlpatterns += doc_urls
